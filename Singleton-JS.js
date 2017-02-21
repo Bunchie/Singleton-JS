5d260030b0c0bc1218a384faf3bdbe45b69d2ec1
Vadim Sharai <ziberman.v.s@gmail.com>
2017-02-21 22:51:14 +0200

    Singelton-JS

---
+++ Singleton-JS.js
@@ -0,0 +1,30 @@
+/**
+ * Created by DELL on 21.02.2017.
+ */
+
+var Singleton = (function() {
+    
+    var instance;
+
+    // Приватные методы и свойства
+    // ...
+
+    // Конструктор
+    function Singleton() {
+        if ( !instance )
+            instance = this;
+        else return instance;
+
+        // Публичные свойства
+    }
+
+    // Публичные методы
+    Singleton.prototype.test = function() {return 1};
+
+    return Singleton;
+
+})()
+
+console.log( new Singleton() === new Singleton() );
+
+console.log(Singleton().test());
